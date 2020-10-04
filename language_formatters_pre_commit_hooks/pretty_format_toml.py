690ce90870be895cb7c372c1f03d1cc3a41cac8c
Samuele Maci <macisamuele@gmail.com>
2020-10-04 13:08:46 +0100

    Update `pretty-format-toml` to depend on `tomlkit` instead of `toml`

--- language_formatters_pre_commit_hooks/pretty_format_toml.py
+++ language_formatters_pre_commit_hooks/pretty_format_toml.py
@@ -8,11 +8,244 @@ import io
 import sys
 import typing
 
-from toml import dumps
-from toml import loads
-from toml import TomlDecodeError
+import tomlkit
+from tomlkit import api
+from tomlkit.container import Container
+from tomlkit.exceptions import ParseError
+from tomlkit.items import AoT
+from tomlkit.items import Array
+from tomlkit.items import Bool
+from tomlkit.items import Comment
+from tomlkit.items import Float
+from tomlkit.items import InlineTable
+from tomlkit.items import Integer
+from tomlkit.items import Item
+from tomlkit.items import Key
+from tomlkit.items import String
+from tomlkit.items import Table
+from tomlkit.items import Whitespace
 
-from language_formatters_pre_commit_hooks.utils import remove_trailing_whitespaces_and_set_new_line_ending
+
+if getattr(typing, "TYPE_CHECKING", False):
+    from tomlkit.toml_document import TOMLDocument
+
+    T = typing.TypeVar("T")
+
+
+class PrettyTomlDocument:
+    @staticmethod
+    def __swap_objects_inplace(obj1, obj2):
+        # type: (T, T) -> None
+        """
+        Python trick to swap two objects without changing their memory address.
+        This is needed if we want to swap the content of an object without
+        having the handle of the object/variable pointing to them
+
+        NOTE: It depends on the object having `__dict__`
+        """
+        obj1.__dict__, obj2.__dict__ = obj2.__dict__, obj1.__dict__
+
+    def __init__(self, toml_content):
+        # type: (typing.Text) -> None
+
+        self.root_container = Container()
+        # Ensure that the TOMLDocument (extending Container) is stored as container
+        # We're doing it such that dumping allows us to sort the keys
+        self.__swap_objects_inplace(self.root_container, tomlkit.parse(toml_content))
+
+    def _prettify_array(self, value):
+        # type: (Array) -> None
+        # Multiline support require some tricking to ensure that is pretty.
+        # We might be doing it in the future, but not for now
+        value.multiline(False)
+        # To prettify an array we need to have access to `_value` private attribute
+        # This is needed because there is no concrete access to the internal values
+        inner_values = [
+            api.ws(", ") if isinstance(v, Whitespace) else self._prettify_value(v)
+            for v in value._value
+            if not isinstance(v, Whitespace) or v.value.strip()
+        ]
+        value._value = inner_values
+
+    def _prettify_aot(self, value):
+        # type: (AoT) -> None
+        for container in value.value:
+            self._prettify_container(container)
+
+    def _prettify_bool(self, value):
+        # type: (Bool) -> None
+        pass
+
+    def _prettify_comment(self, value):
+        # type: (Comment) -> None
+        value.indent(0)
+
+    def _prettify_value(self, value):
+        # type: (T) -> T
+        self._prettify_indentation(value, 0)  # TODO: Enable indentation support (optional)
+        self._prettify_inline_comment(value)
+
+        if isinstance(value, AoT):
+            self._prettify_aot(value)
+        elif isinstance(value, Array):
+            self._prettify_array(value)
+        if isinstance(value, Bool):
+            self._prettify_bool(value)
+        elif isinstance(value, Comment):  # TODO: check later if needed
+            self._prettify_comment(value)
+        elif isinstance(value, InlineTable):
+            self._prettify_inline_table(value)
+        elif isinstance(value, Integer):
+            self._prettify_integer(value)
+        elif isinstance(value, Float):
+            self._prettify_float(value)
+        elif isinstance(value, String):
+            self._prettify_string(value)
+        elif isinstance(value, Table):
+            self._prettify_table(value)
+
+        if isinstance(value, (AoT, InlineTable, Table)):
+            # TODO: Sort table keys
+            pass
+
+        self._prettify_trail(value)
+        return value
+
+    def _prettify_container(self, value):
+        # type: (Container) -> None
+        for key, value in value.body:
+            if key is not None:
+                self._prettify_key(key)
+            if value is not None:
+                self._prettify_value(value)
+
+    def _prettify_key(self, value):
+        # type: (Key) -> None
+        # Create a new equivalent key via the APIs
+        # Doing it enables us to create the key as we like instead of being
+        # dependend on how it looked in the input
+        # The preference would be to have bare keys (no quotes) whenever possible
+        self.__swap_objects_inplace(value, api.key(value.key.strip()))
+
+    def _prettify_indentation(self, value, indent_level):
+        # type: (Item, int) -> None
+        # Indent the item to the defined level
+        if isinstance(value, Whitespace):
+            return
+
+        value.indent(indent_level)
+
+    def _prettify_inline_table(self, value):
+        # type: (InlineTable) -> None
+        self._prettify_container(value.value)
+
+    def _prettify_inline_comment(self, value):
+        # type: (Item) -> None
+        if isinstance(value, Whitespace):
+            return
+
+        if not value.trivia.comment:
+            return
+
+        # Ensure that a comment will always start with `# `
+        comment_string = value.trivia.comment.lstrip("#").strip()
+        value.comment(comment_string)
+        # An inline comment will have 2 leading spaces (to separe the value from the comment)
+        value.trivia.comment_ws = "  "
+
+    def _prettify_integer(self, value):
+        # type: (Integer) -> None
+        str_value = value.as_string()
+
+        is_zero = value == 0
+        is_binary = str_value.startswith("0b")
+        is_octal = str_value.startswith("0o")
+        is_hexadecimal = str_value.startswith("0x")
+
+        # Remove separators
+        str_value = str_value.replace("_", "")
+
+        if str_value.startswith("+"):
+            # Leading '+' does not provide information, removing it
+            str_value = str_value[1:]
+
+        if is_hexadecimal:
+            # We're dealing with hexadecimal numbers, let's ensure a common format
+            str_value = "0x{}".format(str_value[2:].upper())
+
+        if (is_binary or is_octal or is_hexadecimal) and str_value[2] == "0":
+            # The number has leading 0s, we're removing them
+            str_value = "{}{}".format(str_value[:2], str_value[2:].lstrip("0"))
+
+        if is_zero:
+            str_value = "0"
+
+        # Sadly we don't have and API that allows to craeate an integer with a given format
+        # So we go through the parser
+        new_value = tomlkit.parse("key = {}".format(str_value))["key"]
+        self.__swap_objects_inplace(value, new_value)
+
+    def _prettify_float(self, value):
+        # type: (Float) -> None
+        str_value = value.as_string()
+
+        is_zero = value == 0.0
+        is_exponential = "e" in str_value or "E" in str_value
+        has_trailing_zeros = str_value.endswith("0")
+
+        # Remove separators
+        str_value = str_value.replace("_", "")
+
+        if str_value.startswith("+"):
+            # Leading '+' does not provide information, removing it
+            str_value = str_value[1:]
+
+        if is_exponential:
+            # We're dealing with exponential format, let's ensure a common format
+            str_value = str_value.lower()
+
+            if "e0" in str_value:
+                # The exponent has leading 0s
+                number, exponent = str_value.split("e")
+                str_value = "{}e{}".format(number, int(exponent))
+
+        if has_trailing_zeros:
+            # The number has trailing 0s, we're removing them while preserving the first
+            str_value = str_value.rstrip("0")
+            if str_value.endswith("."):
+                # A number as "1." is not valid, so adding a traling 0
+                str_value = "{}0".format(str_value)
+
+        if is_zero:
+            str_value = "0.0"
+
+        # Sadly we don't have and API that allows to craeate an integer with a given format
+        # So we go through the parser
+        new_value = tomlkit.parse("key = {}".format(str_value))["key"]
+        self.__swap_objects_inplace(value, new_value)
+
+    def _prettify_string(self, value):
+        # type: (String) -> None
+        pass
+
+    def _prettify_table(self, value):
+        # type: (Table) -> None
+        self._prettify_container(value.value)
+
+    def _prettify_trail(self, value):
+        # type: (Item) -> None
+        if isinstance(value, Whitespace):
+            return
+        value.trivia.trail = "\n"
+
+    def prettify(self):
+        # type: () -> None
+        self._prettify_container(self.root_container)
+
+    def dumps(self):
+        # type: () -> str
+        self.prettify()
+        return self.root_container.as_string()  # type: ignore  # for some reason mypy does not recognise the correct return type  # noqa: E501
 
 
 def pretty_format_toml(argv=None):
@@ -24,7 +257,6 @@ def pretty_format_toml(argv=None):
         dest="autofix",
         help="Automatically fixes encountered not-pretty-formatted files",
     )
-
     parser.add_argument("filenames", nargs="*", help="Filenames to fix")
     args = parser.parse_args(argv)
 
@@ -35,22 +267,24 @@ def pretty_format_toml(argv=None):
             string_content = "".join(input_file.readlines())
 
         try:
-            prettified_content = remove_trailing_whitespaces_and_set_new_line_ending(
-                dumps(loads(string_content)),
-            )
-
-            if string_content != prettified_content:
-                print("File {} is not pretty-formatted".format(toml_file))
-
-                if args.autofix:
-                    print("Fixing file {}".format(toml_file))
-                    with io.open(toml_file, "w", encoding="UTF-8") as output_file:
-                        output_file.write(prettified_content)
-
-                status = 1
-        except TomlDecodeError:
+            pretty_toml_doc = PrettyTomlDocument(toml_content=string_content)
+        except ParseError as error:
             print("Input File {} is not a valid TOML file".format(toml_file))
-            return 1
+            print(error, file=sys.stderr)
+            status = 1
+            continue
+
+        prettified_content = pretty_toml_doc.dumps()
+
+        if string_content != prettified_content:
+            print("File {} is not pretty-formatted".format(toml_file))
+
+            if args.autofix:
+                print("Fixing file {}".format(toml_file))
+                with io.open(toml_file, "w", encoding="UTF-8") as output_file:
+                    output_file.write(prettified_content)
+
+            status = 1
 
     return status
 
--- tests/pretty_format_toml_test.py
+++ tests/pretty_format_toml_test.py
@@ -6,9 +6,24 @@ from __future__ import unicode_literals
 import os
 
 import pytest
+from tomlkit import api
+from tomlkit.items import AoT
+from tomlkit.items import Array
+from tomlkit.items import Bool
+from tomlkit.items import Comment
+from tomlkit.items import Float
+from tomlkit.items import InlineTable
+from tomlkit.items import Integer
+from tomlkit.items import Key
+from tomlkit.items import String
+from tomlkit.items import Table
 
 from language_formatters_pre_commit_hooks.pretty_format_toml import pretty_format_toml
+from language_formatters_pre_commit_hooks.pretty_format_toml import PrettyTomlDocument
 from tests import run_autofix_test
+
+
+fail_because_tomlkit = pytest.mark.xfail(reason="Moving to tomlkit requires the implementation of some custom handling")
 
 
 @pytest.fixture(autouse=True)
@@ -19,6 +34,290 @@ def change_dir():
         yield
     finally:
         os.chdir(working_directory)
+
+
+class TestTomlKitPrettifiers(object):
+    @pytest.fixture
+    def doc(self):
+        return PrettyTomlDocument("")
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            ("[[aot]]\nkey = 'value'", "[[aot]]\nkey = 'value'\n"),
+            ("[[aot]]\nkey = +0", "[[aot]]\nkey = 0\n"),  # Ensure that we're prettifying the content as well
+        ),
+    )
+    def test__prettify_aot(self, input_string, expected_output):
+        doc = PrettyTomlDocument(input_string)
+        # We need to access the body instead of using `api.value` because
+        # the discrimination between Table and AoT depends on the outside context
+        value = doc.root_container.body[0][1]
+        # Quick "runtime" type check to make sure we're not testing weird cases
+        assert isinstance(value, AoT)
+        doc._prettify_aot(value)
+        assert doc.dumps() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            # Ensure that we have a space after the separator (comma)
+            ("[1, 2,3]", "[1, 2, 3]"),
+            # Ensure that nested are acutally prettified
+            ("[1, 2,0xa]", "[1, 2, 0xA]"),
+            # Arrays are in a single line
+            # For some reason multiline is not well parsed
+            # TODO: We might want to fix this in the future
+            ("[\n    1,\n\n    2,\n    0xa]", "[1, 2, 0xA]"),
+        ),
+    )
+    def test__prettify_array(self, doc, input_string, expected_output):
+        value = api.value(input_string)
+        # Quick "runtime" type check to make sure we're not testing weird cases
+        assert isinstance(value, Array)
+        doc._prettify_array(value)
+        assert value.as_string() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            ("true", "true"),
+            ("false", "false"),
+        ),
+    )
+    def test__prettify_bool(self, doc, input_string, expected_output):
+        value = api.value(input_string)
+        # Quick "runtime" type check to make sure we're not testing weird cases
+        assert isinstance(value, Bool)
+        doc._prettify_bool(value)
+        assert value.as_string() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            ("# This is a comment", "# This is a comment"),
+            ("    # This is a comment", "# This is a comment"),
+        ),
+    )
+    def test__prettify_comment(self, input_string, expected_output):
+        doc = PrettyTomlDocument(input_string)
+        value = doc.root_container.body[0][1]
+        # Quick "runtime" type check to make sure we're not testing weird cases
+        assert isinstance(value, Comment)
+        doc._prettify_comment(value)
+        assert value.as_string() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            ("key", "key"),
+            ("'key'", "key"),
+            ('"key"', "key"),
+            ('"key with space"', '"key with space"'),
+            ("'key with space'", '"key with space"'),
+        ),
+    )
+    def test__prettify_key(self, input_string, expected_output):
+        doc = PrettyTomlDocument("{} = 1".format(input_string))
+        key = doc.root_container.body[0][0]  # Extract the key from the tomlkit parsed document
+        # Quick "runtime" type check to make sure we're not testing weird cases
+        assert isinstance(key, Key)
+        doc._prettify_key(key)
+        assert key.as_string() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, indent_level, expected_output",
+        (
+            ("a = 1", 0, "a = 1"),
+            ("a = 1", 1, " a = 1"),
+            ("    a = 1", 1, " a = 1"),
+        ),
+    )
+    def test__prettify_indentation(self, input_string, indent_level, expected_output):
+        doc = PrettyTomlDocument(input_string)
+        for _, value in doc.root_container.items():
+            doc._prettify_indentation(value, indent_level)
+        assert doc.root_container.as_string() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            ("a = 1# comment", "a = 1  # comment"),
+            ("a = 1  #comment", "a = 1  # comment"),
+            ("a = 1    #    comment", "a = 1  # comment"),
+        ),
+    )
+    def test__prettify_inline_comment(self, input_string, expected_output):
+        doc = PrettyTomlDocument(input_string)
+        for _, value in doc.root_container.items():
+            doc._prettify_inline_comment(value)
+        assert doc.root_container.as_string() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            ("table = { key = 'table' }", "table = { key = 'table' }\n"),
+            ("table = { key = +0 }", "table = { key = 0 }\n"),  # Ensure that we're prettifying the content as well
+        ),
+    )
+    def test__prettify_inline_table(self, input_string, expected_output):
+        doc = PrettyTomlDocument(input_string)
+        value = doc.root_container.body[0][1]
+        assert isinstance(value, InlineTable)
+        doc._prettify_inline_table(value)
+        assert doc.dumps() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            ("+0", "0"),
+            ("-0", "0"),
+            ("0", "0"),
+            ("1", "1"),
+            ("+2", "2"),
+            ("-3", "-3"),
+            ("0b0", "0"),
+            ("0b101", "0b101"),
+            ("0o0", "0"),
+            ("0o00", "0"),
+            ("0o234", "0o234"),
+            ("0x0", "0"),
+            ("0x00", "0"),
+            ("0xabc", "0xABC"),
+            # Separators
+            ("4_5", "45"),
+            ("0xab_c", "0xABC"),
+            # Leading 0s
+            ("0b01", "0b1"),
+            ("0o02", "0o2"),
+            ("0x0F", "0xF"),
+        ),
+    )
+    def test__prettify_integer(self, doc, input_string, expected_output):
+        value = api.value(input_string)
+        # Quick "runtime" type check to make sure we're not testing weird cases
+        assert isinstance(value, Integer)
+        doc._prettify_integer(value)
+        assert value.as_string() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            ("+0.00", "0.0"),
+            ("-0.00", "0.0"),
+            ("1.9", "1.9"),
+            ("+2.1", "2.1"),
+            ("-3.2", "-3.2"),
+            ("0e2", "0.0"),
+            ("-0e23141", "0.0"),
+            ("1e2", "1e2"),
+            ("3E4", "3e4"),
+            # Separators
+            ("1_2.3_4", "12.34"),
+            # Leading 0s
+            ("3e04", "3e4"),
+            # Trailing 0s
+            ("5.0", "5.0"),
+            ("67.890", "67.89"),
+        ),
+    )
+    def test__prettify_float(self, doc, input_string, expected_output):
+        value = api.value(input_string)
+        # Quick "runtime" type check to make sure we're not testing weird cases
+        assert isinstance(value, Float)
+        doc._prettify_float(value)
+        assert value.as_string() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            ("'something'", "'something'"),
+            ('"something"', '"something"'),
+            (
+                '''"""something on
+multiple lines"""''',
+                '''"""something on
+multiple lines"""''',
+            ),
+            (
+                """'''something on
+multiple lines'''""",
+                """'''something on
+multiple lines'''""",
+            ),
+        ),
+    )
+    def test__prettify_string(self, doc, input_string, expected_output):
+        value = api.value(input_string)
+        # Quick "runtime" type check to make sure we're not testing weird cases
+        assert isinstance(value, String)
+        doc._prettify_string(value)
+        assert value.as_string() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            ("[table]\n", "[table]\n"),
+            ("[table]\nkey = 'value'\n", "[table]\nkey = 'value'\n"),
+            ("[table]\n    key = 'value'\n", "[table]\nkey = 'value'\n"),
+            ("[table]\nkey = +0\n", "[table]\nkey = 0\n"),  # Ensure that we're prettifying the content as well
+        ),
+    )
+    def test__prettify_table(self, input_string, expected_output):
+        doc = PrettyTomlDocument(input_string)
+        value = doc.root_container.body[0][1]
+        # Quick "runtime" type check to make sure we're not testing weird cases
+        assert isinstance(value, Table)
+        doc._prettify_table(value)
+        assert doc.dumps() == expected_output
+
+    @pytest.mark.parametrize(
+        "input_string, expected_output",
+        (
+            # TODO: Add cases with multiple consecutive white lines.
+            # TODO: Add test case with empty lines at the begin or end of the document
+            # Boolean formatting
+            ("key = true", "key = true\n"),
+            ("key = false", "key = false\n"),
+            # Comment formatting
+            ("# This is a comment", "# This is a comment\n"),
+            (" # This is a comment", "# This is a comment\n"),
+            ("key = 'value'# This is a comment", "key = 'value'  # This is a comment\n"),
+            ("key = 'value'  # This is a comment", "key = 'value'  # This is a comment\n"),
+            ("key = 'value'    # This is a comment", "key = 'value'  # This is a comment\n"),
+            # Key formatting
+            ("key = 'value'", "key = 'value'\n"),
+            ("'key' = 'value'", "key = 'value'\n"),
+            ("'key'= 'value'", "key = 'value'\n"),
+            ("key.with.dots = 'value'", "[key.with]\ndots = 'value'\n"),
+            # Integer formatting
+            ("key = 1", "key = 1\n"),
+            ("key = +1", "key = 1\n"),
+            ("key= 0xab", "key = 0xAB\n"),
+            # Float formatting
+            ("key = 1.0", "key = 1.0\n"),
+            ("key = +1.20", "key = 1.2\n"),
+            ("key= 1e02", "key = 1e2\n"),
+            # String formatting
+            ("key = 'something'", "key = 'something'\n"),
+            ('key = "something"', 'key = "something"\n'),
+            # Table formatting
+            ("[table]\n", "[table]\n"),
+            ("[table]\nkey = 'value'\n", "[table]\nkey = 'value'\n"),
+            ("[table]\n    'key' = 0xabc\n", "[table]\nkey = 0xABC\n"),
+            # Array of Table formatting
+            ("[[table]]\n    'key' = 0xabc\n", "[[table]]\nkey = 0xABC\n"),
+            ("[[table]]\n    'key' = 0xabc\n[[table]]\nother = false\n", "[[table]]\nkey = 0xABC\n[[table]]\nother = false\n"),
+            # Array formatting
+            ("key = [1, 2,3]", "key = [1, 2, 3]\n"),
+            ("key = [1, 2,{a = 1}]", "key = [1, 2, {a = 1}]\n"),
+            # Inline Table formatting
+            ("key = {inner_key = 0xa}", "key = {inner_key = 0xA}\n"),
+        ),
+    )
+    def test_dumps(self, input_string, expected_output):
+        doc = PrettyTomlDocument(input_string)
+        assert doc.dumps() == expected_output
 
 
 @pytest.mark.parametrize(
@@ -34,6 +333,7 @@ def test_pretty_format_toml(filename, expected_retval):
     assert pretty_format_toml([filename]) == expected_retval
 
 
+@fail_because_tomlkit
 def test_pretty_format_toml_autofix(tmpdir):
     run_autofix_test(
         tmpdir,
